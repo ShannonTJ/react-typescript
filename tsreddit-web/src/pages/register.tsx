dbfee28b3500bb1ffab48b12cb7bc546e06b2682
Shannon TJ <shannontj@shaw.ca>
2021-03-02 11:16:20 -0700

    import formik and form on register page

--- tsreddit-web/src/pages/register.tsx
+++ tsreddit-web/src/pages/register.tsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import {Formik, Form} from 'formik'
+import { FormControl, FormErrorMessage, FormLabel, Input } from '@chakra-ui/react'
 
 interface registerProps {
 
 }
 
+
+
+
 const Register: React.FC<registerProps> = ({}) => {
-    return (<div>register page</div>);
+    return (
+    <Formik initialValues={{username: "", password: ""}}
+    onSubmit={(values) => {console.log(values)}}> 
+        {(values, handleChange) => 
+        (
+        <Form>              
+            <FormControl>
+                <FormLabel htmlFor="username">Username</FormLabel>
+                <Input value={values.username} onChange={handleChange} id="username" placeholder="username" />
+                {/* <FormErrorMessage>{form.errors.name}</FormErrorMessage> */}
+              </FormControl>
+              </Form>
+              )}
+    </Formik>)
 
 }
 
