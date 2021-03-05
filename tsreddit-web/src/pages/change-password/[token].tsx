246255ff586428c0060d6cc68144aaf42aea12ae
Shannon TJ <shannontj@shaw.ca>
2021-03-04 18:27:42 -0700

    change password form

--- tsreddit-web/src/pages/change-password/[token].tsx
+++ tsreddit-web/src/pages/change-password/[token].tsx
@@ -1,8 +1,48 @@
 import React from "react";
 import { NextPage } from "next";
+import { Wrapper } from "../../components/Wrapper";
+import { Form, Formik } from "formik";
+import { InputField } from "../../components/InputField";
+import { Button } from "@chakra-ui/react";
 
 export const ChangePassword: NextPage<{ token: string }> = ({ token }) => {
-  return <div>token is {token}</div>;
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ newPassword: "" }}
+        onSubmit={async (values, { setErrors }) => {
+          //   const response = await login(values);
+          //   if (response.data?.login.errors) {
+          //     setErrors(toErrorMap(response.data.login.errors));
+          //   } else if (response.data?.login.user) {
+          //     //registration worked
+          //     router.push("/");
+          //   }
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="newPassword"
+              placeholder="new password"
+              label="New Password"
+              type="password"
+            />
+
+            <Button
+              isLoading={isSubmitting}
+              mt={4}
+              type="submit"
+              colorScheme="blue"
+              color="white"
+            >
+              Change Password
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
 };
 
 ChangePassword.getInitialProps = ({ query }) => {
