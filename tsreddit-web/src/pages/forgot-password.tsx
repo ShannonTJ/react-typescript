83f580a2e4c4cbfe01bd78a53015fbbb1a7eba13
Shannon TJ <shannontj@shaw.ca>
2021-03-04 20:35:23 -0700

    forgot password can be navigated to and works

--- tsreddit-web/src/pages/forgot-password.tsx
+++ tsreddit-web/src/pages/forgot-password.tsx
@@ -1,7 +1,56 @@
-import React from "react";
+import { Box, Flex, Link, Button } from "@chakra-ui/react";
+import { create } from "domain";
+import { Formik, Form } from "formik";
+import { withUrqlClient } from "next-urql";
+import router from "next/dist/next-server/lib/router/router";
+import React, { useState } from "react";
+import { InputField } from "../components/InputField";
+import { Wrapper } from "../components/Wrapper";
+import { useForgotPasswordMutation } from "../generated/graphql";
+import { createUrqlClient } from "../utils/createUrqlClient";
+import { toErrorMap } from "../utils/toErrorMap";
+import login from "./login";
 
 export const ForgotPassword: React.FC<{}> = ({}) => {
-  return <div>forgot password</div>;
+  const [complete, setComplete] = useState(false);
+  const [, forgotPassword] = useForgotPasswordMutation();
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ email: "" }}
+        onSubmit={async (values) => {
+          await forgotPassword(values);
+          setComplete(true);
+        }}
+      >
+        {({ isSubmitting }) =>
+          complete ? (
+            <Box>
+              If an account with that email exists, we've sent you a reset link.
+            </Box>
+          ) : (
+            <Form>
+              <InputField
+                name="email"
+                placeholder="email"
+                label="Email"
+                type="email"
+              />
+              <Button
+                isLoading={isSubmitting}
+                mt={4}
+                type="submit"
+                colorScheme="blue"
+                color="white"
+              >
+                Confirm
+              </Button>
+            </Form>
+          )
+        }
+      </Formik>
+    </Wrapper>
+  );
 };
 
-export default ForgotPassword;
+export default withUrqlClient(createUrqlClient)(ForgotPassword);
