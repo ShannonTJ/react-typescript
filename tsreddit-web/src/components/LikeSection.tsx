33d982b19929f5e1c022f44875a59cd51042d6c4
Shannon TJ <shannontj@shaw.ca>
2021-03-07 13:44:06 -0700

    vote mutation graphql and like section loading

--- tsreddit-web/src/components/LikeSection.tsx
+++ tsreddit-web/src/components/LikeSection.tsx
@@ -1,18 +1,34 @@
 import { ChevronUpIcon, ChevronDownIcon } from "@chakra-ui/icons";
 import { Flex, IconButton } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
 import { PostSnippetFragment } from "../generated/graphql";
+import { useVoteMutation } from "../generated/graphql";
 
 interface LikeSectionProps {
   post: PostSnippetFragment;
 }
 
 export const LikeSection: React.FC<LikeSectionProps> = ({ post }) => {
+  const [{ fetching, operation }, vote] = useVoteMutation();
   return (
     <Flex direction="column" justifyContent="center" alignItems="center" mr={4}>
-      <IconButton aria-label="like post" icon={<ChevronUpIcon />} />
+      <IconButton
+        onClick={async () => {
+          await vote({ postId: post.id, value: 1 });
+        }}
+        isLoading={fetching && operation?.variables?.value === 1}
+        aria-label="like post"
+        icon={<ChevronUpIcon />}
+      />
       {post.points}
-      <IconButton aria-label="unlike post" icon={<ChevronDownIcon />} />
+      <IconButton
+        onClick={async () => {
+          await vote({ postId: post.id, value: -1 });
+        }}
+        isLoading={fetching && operation?.variables?.value === -1}
+        aria-label="unlike post"
+        icon={<ChevronDownIcon />}
+      />
     </Flex>
   );
 };
