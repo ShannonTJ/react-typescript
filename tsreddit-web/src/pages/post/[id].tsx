243c4ca321be4506be7d6604bc403eec86666d48
Shannon TJ <shannontj@shaw.ca>
2021-03-08 16:32:36 -0700

    add post query to web server

--- tsreddit-web/src/pages/post/[id].tsx
+++ tsreddit-web/src/pages/post/[id].tsx
@@ -2,10 +2,19 @@ import React from "react";
 import { withUrqlClient } from "next-urql";
 import { useRouter } from "next/router";
 import { createUrqlClient } from "../../utils/createUrqlClient";
+import { usePostQuery } from "../../generated/graphql";
 
 export const Post = ({}) => {
   const router = useRouter();
-  router.query.id;
+  const intId =
+    typeof router.query.id === "string" ? parseInt(router.query.id) : -1;
+  const [{ data, fetching }] = usePostQuery({
+    pause: intId === -1,
+    variables: {
+      id: intId,
+    },
+  });
+
   return <div></div>;
 };
 
