fa8fbb80e0406cb3205abd3c11607ded80e25f4f
Shannon TJ <shannontj@shaw.ca>
2021-03-08 16:40:41 -0700

    add header link to individual posts

--- tsreddit-web/src/pages/post/[id].tsx
+++ tsreddit-web/src/pages/post/[id].tsx
@@ -3,19 +3,32 @@ import { withUrqlClient } from "next-urql";
 import { useRouter } from "next/router";
 import { createUrqlClient } from "../../utils/createUrqlClient";
 import { usePostQuery } from "../../generated/graphql";
+import { Layout } from "../../components/Layout";
 
 export const Post = ({}) => {
   const router = useRouter();
   const intId =
     typeof router.query.id === "string" ? parseInt(router.query.id) : -1;
-  const [{ data, fetching }] = usePostQuery({
+  const [{ data, error, fetching }] = usePostQuery({
     pause: intId === -1,
     variables: {
       id: intId,
     },
   });
 
-  return <div></div>;
+  if (fetching) {
+    return (
+      <Layout>
+        <div>Loading...</div>
+      </Layout>
+    );
+  }
+
+  if (error) {
+    return <div>{error.message}</div>;
+  }
+
+  return <Layout>{data?.post?.text}</Layout>;
 };
 
 export default withUrqlClient(createUrqlClient, { ssr: true })(Post);
