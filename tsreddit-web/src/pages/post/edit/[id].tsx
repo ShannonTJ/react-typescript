dbb845d24971896d2bf70cab7749bbf13edc7eb4
Shannon TJ <shannontj@shaw.ca>
2021-03-08 20:01:01 -0700

    edit post setup

--- tsreddit-web/src/pages/post/edit/[id].tsx
+++ tsreddit-web/src/pages/post/edit/[id].tsx
@@ -2,9 +2,55 @@ import React from "react";
 import { withUrqlClient } from "next-urql";
 import { createUrqlClient } from "../../../utils/createUrqlClient";
 import { Layout } from "../../../components/Layout";
+import { Box, Button } from "@chakra-ui/react";
+import { Formik, Form } from "formik";
+import { useRouter } from "next/router";
+import { InputField } from "../../../components/InputField";
+import { Wrapper } from "../../../components/Wrapper";
+import { usePostQuery } from "../../../generated/graphql";
 
 export const EditPost = ({}) => {
-  return <Layout>edit page</Layout>;
+  const router = useRouter();
+  const [] = usePostQuery({ variables: { id: router } });
+  return (
+    <Layout variant="small">
+      <Wrapper variant="small">
+        <Formik
+          initialValues={{ title: "", text: "" }}
+          onSubmit={async (values) => {
+            // const { error } = await createPost({ input: values });
+            // if (!error) {
+            //   router.push("/");
+            // }
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form>
+              <InputField name="title" placeholder="title" label="Title" />
+              <Box mt={4}>
+                <InputField
+                  textarea
+                  name="text"
+                  placeholder="text..."
+                  label="Body"
+                />
+              </Box>
+
+              <Button
+                isLoading={isSubmitting}
+                mt={4}
+                type="submit"
+                colorScheme="blue"
+                color="white"
+              >
+                Update Post
+              </Button>
+            </Form>
+          )}
+        </Formik>
+      </Wrapper>
+    </Layout>
+  );
 };
 
 export default withUrqlClient(createUrqlClient)(EditPost);
