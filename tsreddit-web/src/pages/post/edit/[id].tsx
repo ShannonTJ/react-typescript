a62c57a419b8286b00f1c1968a66eba0e2493db4
Shannon TJ <shannontj@shaw.ca>
2021-03-08 20:14:18 -0700

    update post functionality

--- tsreddit-web/src/pages/post/edit/[id].tsx
+++ tsreddit-web/src/pages/post/edit/[id].tsx
@@ -6,10 +6,23 @@ import { Box, Button } from "@chakra-ui/react";
 import { Formik, Form } from "formik";
 import { InputField } from "../../../components/InputField";
 import { Wrapper } from "../../../components/Wrapper";
-import { useGetPostFromUrl } from "../../../utils/useGetPostFromUrl";
+import {
+  usePostQuery,
+  useUpdatePostMutation,
+} from "../../../generated/graphql";
+import { useGetIntId } from "../../../utils/useGetIntId";
+import { useRouter } from "next/router";
 
 export const EditPost = ({}) => {
-  const [{ data, fetching }] = useGetPostFromUrl();
+  const router = useRouter();
+  const intId = useGetIntId();
+  const [{ data, fetching }] = usePostQuery({
+    pause: intId === -1,
+    variables: {
+      id: intId,
+    },
+  });
+  const [{}, updatePost] = useUpdatePostMutation();
 
   if (fetching) {
     return (
@@ -18,16 +31,27 @@ export const EditPost = ({}) => {
       </Layout>
     );
   }
+
+  if (!data?.post) {
+    return (
+      <Layout>
+        <div>Could not find post</div>
+      </Layout>
+    );
+  }
+
   return (
     <Layout variant="small">
       <Wrapper variant="small">
         <Formik
-          initialValues={{ title: "", text: "" }}
+          initialValues={{ title: data.post.title, text: data.post.text }}
           onSubmit={async (values) => {
             // const { error } = await createPost({ input: values });
             // if (!error) {
             //   router.push("/");
             // }
+            await updatePost({ id: intId, ...values });
+            router.push("/");
           }}
         >
           {({ isSubmitting }) => (
@@ -49,7 +73,7 @@ export const EditPost = ({}) => {
                 colorScheme="blue"
                 color="white"
               >
-                Update Post
+                update Post
               </Button>
             </Form>
           )}
